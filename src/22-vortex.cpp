c22e167bba86a07398b6bc2e7e130714db3be7b6
haneulmath <matheopalaz@gmail.com>
2024-11-19 10:31:07 +0100

    test vortex

--- src/22-vortex.cpp
+++ src/22-vortex.cpp
@@ -3,6 +3,19 @@
 int main()
 {
     sil::Image image{"images/logo.png"};
-    
+
+    for (int x{0}; x < image.width(); x++)
+    {
+        for (int y{0}; y < image.height(); y++)
+        {
+            image.pixel(x, y).r = 0.f;
+        }
+    }
+
+    for (glm::vec3& color : image.pixels())
+    {
+        color.r = 0.f;
+        color.b = 0.f;
+    }
     image.save("output/" FILE_NAME ".png");
 }
